744d40d5cfef221404ca2206db1e416bceef4477
Aleksei Lokhin <lokhin.a.a@gmail.com>
2022-09-20 19:54:27 +0300

    Initial Commit

---
+++ СloserToTheTruth/_loserToTheTruthApp.swift
@@ -0,0 +1,17 @@
+//
+//  _loserToTheTruthApp.swift
+//  СloserToTheTruth
+//
+//  Created by Алексей on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct _loserToTheTruthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
